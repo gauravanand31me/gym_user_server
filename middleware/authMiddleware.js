ba0e0e9c53663091f6f7e34cb17f9c32d528b793
Sourav Satyam <sourav.satyamsingh@gmail.com>
2024-09-22 21:12:48 +0530

    Added Authorization header

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const JWT_SECRET = process.env.JWT_SECRET || "Test@1992"
 exports.authMiddleware = async (req, res, next) => {
+  console.log("Headers received", req.headers);
   const token = req.header('Authorization').replace('Bearer ', '');
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
