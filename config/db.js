ef81d6e602e66f0d6ff884375f851c583de3f78b
Sourav Satyam <sourav.satyamsingh@gmail.com>
2024-09-22 13:14:33 +0530

    Added Database details using env

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
-const { Sequelize } = require('sequelize');
+require('dotenv').config(); // Load environment variables from .env file
 
-const sequelize = new Sequelize('fitzoos', 'postgres', 'Sourav@1992', {
-  host: 'localhost',
-  dialect: 'postgres',
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_FITZOO_DATABASE, process.env.DB_FITZOO_USERNAME, process.env.DB_FITZOO_PASSWORD, {
+  host: process.env.DB_FITZOO_HOST,
+  dialect: process.env.DB_FITZOO_DIALECT,
   logging: false,
 });
 
@@ -10,9 +11,6 @@ sequelize.authenticate()
   .then(() => console.log('Database connected'))
   .catch(err => console.log('Error: ' + err));
 
-  sequelize.sync({ alter: true }); // This will update the schema without dropping existing tables
+sequelize.sync({ alter: true }); // This will update the schema without dropping existing tables
 
 module.exports = sequelize;
-
-
-
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+console.log('Loaded environment variables:', process.env.DB_FITZOO_HOST); // Log all env variables
 const express = require('express');
 const sequelize = require('./config/db');
 const bodyParser = require('body-parser');
@@ -11,12 +14,14 @@ const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/gym', gymRoutes);
 app.use('/api/booking', bookingRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/friends', friendRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 sequelize.sync().then(() => {
