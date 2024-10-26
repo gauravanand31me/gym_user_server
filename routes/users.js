5337b4ac0ed7d7ad74286bf85a668093dde66a9c
Sourav Satyam <sourav.satyamsingh@gmail.com>
2024-10-26 22:48:14 +0530

    add api also

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { authMiddleware } = require('../middleware/authMiddleware');
 const { getNearbyUsers } = require('../controllers/getNearbyUsers');
 const upload = require('../middleware/upload');
-const { getIndividualUser, searchUsersByUsernameOrLocation, uploadProfileImage,uploadPostImage,  getUserImage } = require('../controllers/getIndividualUser');
+const { getIndividualUser, searchUsersByUsernameOrLocation, uploadProfileImage,uploadPostImage,  getUserImage, updateFullName } = require('../controllers/getIndividualUser');
 const router = express.Router();
 
 router.get('/nearby-users', authMiddleware, searchUsersByUsernameOrLocation);
@@ -11,4 +11,5 @@ router.get('/search/:username', authMiddleware, searchUsersByUsernameOrLocation)
 router.post('/uploadProfileImage', authMiddleware, upload.single('profileImage'), uploadProfileImage);
 router.post('/uploadImage', authMiddleware, upload.single('postImage'), uploadPostImage);
 router.get('/getImage/:userId', authMiddleware, getUserImage);
+router.put('/update-fullname', authMiddleware, updateFullName);
 module.exports = router;
