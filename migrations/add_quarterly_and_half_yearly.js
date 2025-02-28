34d49ccf5af81f4e6c049d164ebf047f1fbdb92d
Gauravanand32me@gmail.com <sourav.satyamsingh@gmail.com>
2025-02-28 21:18:56 +0530

    Added two additional parameter for enum type

---
+++ migrations/add_quarterly_and_half_yearly.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+      ALTER TYPE "enum_Bookings_type" ADD VALUE IF NOT EXISTS 'quarterly';
+    `);
+    await queryInterface.sequelize.query(`
+      ALTER TYPE "enum_Bookings_type" ADD VALUE IF NOT EXISTS 'halfyearly';
+    `);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Note: Removing ENUM values is more complex and often not supported directly.
+    // You would need to recreate the ENUM type without these values if you wanted to roll this back.
+    console.warn('Down migration for ENUM value removal is not supported automatically.');
+  }
+};
