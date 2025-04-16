539c432afcb6118fe6c52109ea6ff201e2053423
Gauravanand32me@gmail.com <sourav.satyamsin@gmail.com>
2025-04-16 05:37:01 +0530

    some changes to the migrations column

--- migrations/20250415235643-create-post-reactions.js
+++ migrations/20250415235643-create-post-reactions.js
@@ -5,7 +5,7 @@ module.exports = {
     await queryInterface.createTable('PostReactions', {
       id: {
         type: Sequelize.UUID,
-        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+        defaultValue: Sequelize.UUIDV4,
         primaryKey: true,
       },
       userId: {
