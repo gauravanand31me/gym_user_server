01a2ca10572ace81e58be713caee716a44f8f762
Gauravanand32me@gmail.com <sourav.satyamsin@gmail.com>
2025-04-30 01:08:57 +0530

    getting the delete video url

--- controllers/getIndividualUser.js
+++ controllers/getIndividualUser.js
@@ -110,7 +110,7 @@ exports.deleteReel = async (req, res) => {
     // Step 3: Extract S3 Key from video URL
     const videoUrl = reel.videoUrl;
     const streamPrefix = `/api/stream-reel/`;
-
+    console.log("videoUrl", videoUrl);
     if (!videoUrl.startsWith(streamPrefix)) {
       return res.status(400).json({ success: false, message: 'Invalid video URL format.' });
     }
