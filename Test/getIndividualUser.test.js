da742719e57a830548108f4816e575be336ed4b7
Gourav Anand <Gauravanand32me@gmail.com>
2026-03-13 19:09:25 -0700

    Just initial test

--- Test/getIndividualUser.test.js
+++ Test/getIndividualUser.test.js
@@ -1,17 +1,9 @@
 const request = require('supertest');
 const app = require('../server'); // Assuming server.js initializes the app
-const { User } = require('../models/User');
+
 const { sequelize } = require('../config/db');
 
-jest.mock('../models/User');
 
-beforeAll(async () => {
-  await sequelize.sync();
-});
-
-afterAll(async () => {
-  await sequelize.close();
-});
 
 test('Basic Jest Test', () => {
   expect(true).toBe(true);
