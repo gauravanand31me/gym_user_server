f4f773d1d362933565a4cdfcf2fcb62f767128e9
Gourav Anand <Gauravanand32me@gmail.com>
2026-03-13 19:10:56 -0700

    Just a basic test

--- Test/getIndividualUser.test.js
+++ Test/getIndividualUser.test.js
@@ -1,10 +1,3 @@
-const request = require('supertest');
-const app = require('../server'); // Assuming server.js initializes the app
-
-const { sequelize } = require('../config/db');
-
-
-
 test('Basic Jest Test', () => {
   expect(true).toBe(true);
 });
