5faead64823f59ce7bb0a430b351ea1dd0ab2337
Gourav Anand <Gauravanand32me@gmail.com>
2026-03-23 12:30:09 -0700

    updating cloudfront url

--- controllers/updateCloudFrontUrls.js
+++ controllers/updateCloudFrontUrls.js
@@ -3,34 +3,34 @@ const Reel = require('../models/Reel');
 const Feed = require('../models/Feed');
 const User = require('../models/User');
 const sequelize = require('../config/db');
-
+const { Op, Sequelize } = require('sequelize');
 
 const updateCloudFrontUrls = async (req, res) => {
     try {
-        const oldUrl = 'https://d3tfjww6nofv30.cloudfront.net';
-        const newUrl = 'https://d1ggj6jmm6y2ok.cloudfront.net';
+        const oldUrl = 'https://d1ggj6jmm6y2ok.cloudfront.net';
+        const newUrl = 'https://d59q7mzjlaq7y.cloudfront.net';
 
         // Update Feed model
         await Feed.update(
             { imageUrl: sequelize.fn('REPLACE', sequelize.col('imageUrl'), oldUrl, newUrl) },
-            { where: { imageUrl: { [sequelize.Op.like]: `${oldUrl}%` } } }
+            { where: { imageUrl: { [Op.like]: `${oldUrl}%` } } }
         );
 
-        // Update Reels model
-        await Reels.update(
+        // Update Reel model
+        await Reel.update(
             { videoUrl: sequelize.fn('REPLACE', sequelize.col('videoUrl'), oldUrl, newUrl) },
-            { where: { videoUrl: { [sequelize.Op.like]: `${oldUrl}%` } } }
+            { where: { videoUrl: { [Op.like]: `${oldUrl}%` } } }
         );
 
-        await Reels.update(
+        await Reel.update(
             { thumbnailUrl: sequelize.fn('REPLACE', sequelize.col('thumbnailUrl'), oldUrl, newUrl) },
-            { where: { thumbnailUrl: { [sequelize.Op.like]: `${oldUrl}%` } } }
+            { where: { thumbnailUrl: { [Op.like]: `${oldUrl}%` } } }
         );
 
         // Update User model for profile pictures
         await User.update(
             { profilePicUrl: sequelize.fn('REPLACE', sequelize.col('profilePicUrl'), oldUrl, newUrl) },
-            { where: { profilePicUrl: { [sequelize.Op.like]: `${oldUrl}%` } } }
+            { where: { profilePicUrl: { [Op.like]: `${oldUrl}%` } } }
         );
 
         // Update Certificate model for certificates
