702c83c2198f222f2bbcd855535558482e6ae988
Gourav Anand <Gauravanand32me@gmail.com>
2026-08-01 18:37:57 -0700

    delete anything from inslam khan

--- seeders/deleteSpecificReels.js
+++ seeders/deleteSpecificReels.js
@@ -2,22 +2,24 @@ require('dotenv').config({ path: require('path').join(__dirname, '../.env') });
 
 const sequelize = require('../config/db');
 
-const IDS = [
-  '556e2b48-93dc-41e6-9eff-7be3f103dcf6',
-  'e5fc0f87-9d5f-4549-b4f4-974c66b6d51d',
-  '8cb49356-cab1-409e-9622-ceb951bf6ee6',
-  'ce2a894a-6ae2-49f3-a304-15b65f52100d',
-  'fa266ed5-1c12-4c76-a190-2454cde2e55f',
-  '4b377ebe-502d-4f5d-ae61-5c7c8c9a4bd0',
-];
-
 async function run() {
   await sequelize.authenticate();
 
-  await sequelize.query(`DELETE FROM "Reels" WHERE id = ANY($1::uuid[])`, { bind: [IDS] });
+  // Find Islam Khan's user
+  const [users] = await sequelize.query(
+    `SELECT id, full_name FROM "Users" WHERE full_name ILIKE '%islam%khan%'`
+  );
+  console.log('Found users:', users);
+
+  if (!users.length) { console.log('User not found'); process.exit(0); }
+
+  const userId = users[0].id;
+  console.log('Deleting for userId:', userId);
+
+  await sequelize.query(`DELETE FROM "Reels" WHERE "userId" = $1`, { bind: [userId] });
   console.log('Reels deleted');
 
-  await sequelize.query(`DELETE FROM "Feeds" WHERE id = ANY($1::uuid[])`, { bind: [IDS] });
+  await sequelize.query(`DELETE FROM "Feeds" WHERE "userId" = $1`, { bind: [userId] });
   console.log('Feeds deleted');
 
   process.exit(0);
